32517b37ed3b2ecef56dd09af34645090c9787c1
Ryan Kirby <hiddenkirby@gmail.com>
2024-03-11 14:33:40 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ai-text-to-speech
+# ai-speech-to-text
+
+## This is a sample application that uses a replicate.com model called vaibhavs10/incredibly-fast-whisper to generate speech to text
+
+1. run `python3 -m venv .venv`
+2. `source .venv/bin/activate`
+3. `pip install -r requirements.txt`
+4. `python3 app.py`
---
+++ app.py
@@ -0,0 +1,18 @@
+import replicate
+
+from dotenv import load_dotenv
+# load environment variables from .env file
+load_dotenv()
+
+output = replicate.run(
+    "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
+    input={
+        "task": "transcribe",
+        "audio": "https://replicate.delivery/pbxt/Js2Fgx9MSOCzdTnzHQLJXj7abLp3JLIG3iqdsYXV24tHIdk8/OSR_uk_000_0050_8k.wav",
+        "language": "None",
+        "timestamp": "chunk",
+        "batch_size": 64,
+        "diarise_audio": False
+    }
+)
+print(output)
