567ea867247e8f353f02dce5c555b0fb2853e543
Ryan Kirby <hiddenkirby@gmail.com>
2024-03-11 15:12:03 -0400

    working speech to text

--- README.md
+++ README.md
@@ -5,4 +5,14 @@
 1. run `python3 -m venv .venv`
 2. `source .venv/bin/activate`
 3. `pip install -r requirements.txt`
-4. `python3 app.py`
+4. Make sure you at least have a .env file with the following:
+   REPLICATE_API_TOKEN="<your-api-token>"
+   AWS_ACCESS_KEY="<your-aws-access-key>">"
+   AWS_SECRET_KEY="<your-aws-secret-key>"
+5. `python3 app.py`
+
+## This will spin up a local UI on port 8080
+
+1. Press the record icon to start recording
+2. Press the stop icon to stop recording
+3. You will see the text that was generated from the audio
--- app.py
+++ app.py
@@ -1,18 +1,55 @@
 import replicate
+import boto3
+
+import os
+import tempfile
+from flask import Flask, request, jsonify, render_template
 
 from dotenv import load_dotenv
 # load environment variables from .env file
 load_dotenv()
 
-output = replicate.run(
-    "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
-    input={
-        "task": "transcribe",
-        "audio": "https://replicate.delivery/pbxt/Js2Fgx9MSOCzdTnzHQLJXj7abLp3JLIG3iqdsYXV24tHIdk8/OSR_uk_000_0050_8k.wav",
-        "language": "None",
-        "timestamp": "chunk",
-        "batch_size": 64,
-        "diarise_audio": False
-    }
+bucket_name = os.environ.get("AWS_S3_BUCKET")
+aws_access_key = os.environ.get("AWS_ACCESS_KEY")
+aws_secret = os.environ.get("AWS_SECRET_KEY")
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret
 )
-print(output)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/process-audio', methods=['POST'])
+def process_audio_data():
+    audio_data = request.files['audio'].read()
+
+    with tempfile.NamedTemporaryFile(delete=False,suffix=".wav") as f:
+        f.write(audio_data)
+        f.flush()
+        s3.upload_file(f.name, bucket_name, f.name)
+        temp_audio_uri = f"https://{bucket_name}.s3.amazonaws.com/{f.name}"
+
+        output = replicate.run(
+            "vaibhavs10/incredibly-fast-whisper:3ab86df6c8f54c11309d4d1f930ac292bad43ace52d10c80d87eb258b3c9f79c",
+            input={
+                "task": "transcribe",
+                "audio": temp_audio_uri,
+                "language": "None",
+                "timestamp": "chunk",
+                "batch_size": 64,
+                "diarise_audio": False
+            }
+        )
+        print(output)
+
+        return jsonify({"transcript": output["text"]})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
